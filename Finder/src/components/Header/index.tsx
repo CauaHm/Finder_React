a38ae31bda7fcf1db5727597a573d17220f22b28
Cauã Henrique <cauamh321@gmail.com>
2025-08-15 17:28:07 -0300

    Header

---
+++ Finder/src/components/Header/index.tsx
@@ -0,0 +1,56 @@
+import Styles from "./styles.module.css"
+import logo from "../../assets/images/logoFinder.png"
+
+import { FaBars } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { useState } from "react";
+
+interface NavItem {
+    name: string;
+    href: string;
+    icon?: React.ReactNode;
+    active?: boolean
+}
+
+const NavLinks: NavItem[] = [
+    {name: "Inicio", href: "#", active: true},
+    {name: "Produtos", href: "#"},
+    {name: "Login", href: "#"},
+    {name: "Favoritos", href: "#", icon: <FaHeart />},
+    
+]
+
+export function Header() {
+    const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false)
+
+    const toggleMobileMenu = () => {
+        setisMobileMenuOpen(!isMobileMenuOpen)
+    }
+
+    const renderNavItems = () => {
+        return NavLinks.map((i) => {
+             return <li className={`${Styles.navList_item} ${i.active ? Styles.active : ""}`}><a href={i.href}>{i.icon ? i.icon : i.name}</a></li>
+        })
+    }
+
+    return(
+        <header className={Styles.headerContainer}>
+        <nav className={Styles.headerNavBar}>
+            <a href="#">
+                <div className={Styles.navlogo}><img src={logo} alt="logo Finder" /></div>
+            </a>
+            <ul className={Styles.navList}>
+                {renderNavItems()}
+            </ul>
+            <button onClick={toggleMobileMenu} className={Styles.btn_Mobile}>
+                <i><FaBars /></i>
+            </button>
+        </nav>
+        {isMobileMenuOpen && (
+        <ul className={Styles.navList_Mobile}>
+            {renderNavItems()}
+        </ul>
+        )}
+    </header>
+    )
+}
