7e38addb570e687c28a42d9d65b9b4a0382368d9
Cauã Henrique <cauamh321@gmail.com>
2025-08-18 14:20:55 -0300

    Arrumando components com a lib para a animação

--- Finder/src/components/GenericHtml/index.tsx
+++ Finder/src/components/GenericHtml/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import styles from './styles.module.css';
 
 type GenericHtmlProps = {
@@ -8,9 +9,15 @@ type GenericHtmlProps = {
 
 export function GenericHtml({ titulo, tituloGrifado, children }: GenericHtmlProps) {
   return (
-    <div className={styles.GenericHtml}>
+    <motion.div
+        initial={{ opacity: 0, x: -100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 0.8 }}  
+        className={styles.GenericHtml}
+        >
+
       <h2 className={styles.subTitle}>{titulo} <span className={styles.subTitleSpan}>{tituloGrifado}</span></h2>
       {children}
-    </div>
+    </motion.div>
   );
 }
