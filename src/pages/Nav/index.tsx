214518c825ea7cdf0f75a9ed914efdb2c845281b
Cauã Henrique <cauamh321@gmail.com>
2025-10-05 17:55:59 -0300

    Estruturando

--- src/pages/Nav/index.tsx
+++ src/pages/Nav/index.tsx
@@ -1,13 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Home } from '../Home';
-import { Login } from '../Login';
+import { Home } from "../Home";
+import { Login } from "../Login";
+import { Products } from "../Products";
 
 export function Nav() {
-      return (
+  return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
